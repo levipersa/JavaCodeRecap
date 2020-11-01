22a8f5b9115210ff4ec3c10422ae7efdb4616311
Persa Levente <levipersa@gmail.com>
2020-11-01 11:18:02 +0200

    MultipleClassesInSameFile added

---
+++ src/java/fundamentals/MultipleClassesInSameFile.java
@@ -0,0 +1,21 @@
+package java.fundamentals;
+
+public class MultipleClassesInSameFile {
+
+
+    public class InnerClass{
+
+
+    }
+
+      class InnerClass2 {
+
+
+    }
+
+}
+
+//Compile Time Error Multiple Classes Not Allowed in the same file
+//public class FirstOuterClass{
+//
+//}
